951493db86f6570c1c56d980c3f36ea50129de0d
Mikhail_A <artemev1986@yandex.ru>
2023-04-08 16:20:15 +0500

    fix: added validation NotBlank for email

--- src/main/java/com/example/familybudget/config/SecurityConfig.java
+++ src/main/java/com/example/familybudget/config/SecurityConfig.java
@@ -23,6 +23,8 @@ public class SecurityConfig {
     private static final String REGISTRATION_ENDPOINT = "/registration";
     private static final String AUTHENTICATION_ENDPOINT = "/authentication";
     private static final String ACTIVATE_ENDPOINT = "/activate/*";
+    private static final String RESET_PASSWORD = "/reset-password/**";
+    private static final String CHANGE_PASSWORD = "/change-password/**";
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -40,6 +42,8 @@ public class SecurityConfig {
                 .antMatchers(REGISTRATION_ENDPOINT).permitAll()
                 .antMatchers(ACTIVATE_ENDPOINT).permitAll()
                 .antMatchers(AUTHENTICATION_ENDPOINT).permitAll()
+                .antMatchers(RESET_PASSWORD).permitAll()
+                .antMatchers(CHANGE_PASSWORD).permitAll()
                 .anyRequest().authenticated().and()
                 .exceptionHandling().accessDeniedHandler(accessDeniedHandler)
                 .authenticationEntryPoint(authenticationEntryPoint)
